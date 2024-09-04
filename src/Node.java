cf91b1b1f2161f26a0981c7a618419c1ee2401cb
devAlq <khalidalqdev@gmail.com>
2024-09-04 18:49:46 +0300

    taskDone

---
+++ src/Node.java
@@ -0,0 +1,13 @@
+class Node {
+
+    String question;
+    Node right;
+    Node left;
+
+    public Node(String question) {
+        this.question = question;
+        this.right = null;
+        this.left = null;
+    }
+
+}
